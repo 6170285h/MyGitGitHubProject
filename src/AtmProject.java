6432d17264dccd4c716f197d1b62e8753c84fb5b
Hakan Karaaslan <kayahan_hakan@hotmail.com>
2023-04-26 23:31:30 +0300

    hkn4

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -9,6 +9,19 @@ public class AtmProject {
         //version3
         System.out.println("Git ögrenmek çok kolay");
         //version4
+<<<<<<< Updated upstream
+=======
+
+        System.out.println("hakan branchi kodlarını yazdi.");
+        //version hkn1
+
+        System.out.println("bir kısım kodlar");
+        //hkn2
+        System.out.println();
+        //hkn3
+        System.out.println();
+
+>>>>>>> Stashed changes
     }
 
 }
